e72143afc2c3f70954363d376acfd4f4c0a2f2d0
websitekay <websitekay@gmail.com>
2020-01-16 00:11:46 -0800

    Filters
    https://code.angularjs.org/1.2.32/docs/api/ng/filter

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ app.controller('HomeViewController', ['$scope', function ($scope) {
 
 app.controller('ExpensesViewController', ['$scope', function ($scope) {
   $scope.expenses = [
-    {description: "sprout", amount: 10, date: '2014-10-01'},
+    {description: "sprout", amount: 10.001, date: '2014-10-01'},
     {description: "trader joes", amount: 11, date: '2014-10-02'},
     {description: "albertson", amount: 12, date: '2014-10-03'},
     {description: "vons", amount: 13, date: '2014-10-04'},
