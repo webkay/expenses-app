4fa60ac2dbb98ff4bac0dd8941d65603775d59e2
websitekay <websitekay@gmail.com>
2020-01-15 23:45:51 -0800

    one way binding, two way binding

--- js/app.js
+++ js/app.js
@@ -2,4 +2,11 @@ var app = angular.module('expensesApp', []);
 
 app.controller('ExpensesViewController', ['$scope', function ($scope) {
   $scope.phrase = "Hello Moto";
+  $scope.expense = {
+    description: "sprout",
+    amount: 20
+  };
+  $scope.incrementAmount = function () {
+    $scope.expense.amount++;
+  };
 }]);
