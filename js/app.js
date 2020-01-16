544f1cd4e2de5c379f3c4f486a9a8d38cadb72c2
websitekay <websitekay@gmail.com>
2020-01-16 00:01:34 -0800

    multipe expenses, app title

--- js/app.js
+++ js/app.js
@@ -1,12 +1,16 @@
 var app = angular.module('expensesApp', []);
 
+app.controller('HomeViewController', ['$scope', function ($scope) {
+  $scope.appTitle = "Simple Expenses Tracker";
+}]);
+
 app.controller('ExpensesViewController', ['$scope', function ($scope) {
-  $scope.phrase = "Hello Moto";
-  $scope.expense = {
-    description: "sprout",
-    amount: 20
-  };
-  $scope.incrementAmount = function () {
-    $scope.expense.amount++;
-  };
+  $scope.expenses = [
+    {description: "sprout", amount: 10, date: '2014-10-01'},
+    {description: "trader joes", amount: 11, date: '2014-10-02'},
+    {description: "albertson", amount: 12, date: '2014-10-03'},
+    {description: "vons", amount: 13, date: '2014-10-04'},
+    {description: "rite aid", amount: 14, date: '2014-10-05'},
+    {description: "pavilion", amount: 15, date: '2014-10-06'}
+  ];
 }]);
