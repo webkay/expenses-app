57a884e173da7eb7bcf4cf9750d8ed8d6fa4bfb5
websitekay <websitekay@gmail.com>
2020-01-18 18:54:57 -0800

    use Expenses service object

--- js/app.js
+++ js/app.js
@@ -23,12 +23,10 @@ app.config(function ($routeProvider) {
     });
 });
 
-app.controller('HomeViewController', ['$scope', function ($scope) {
-  $scope.appTitle = "Simple Expenses Tracker";
-}]);
+app.factory('Expenses', function () {
+  var service = {};
 
-app.controller('ExpensesViewController', ['$scope', function ($scope) {
-  $scope.expenses = [
+  service.entries = [
     {description: "sprout", amount: 10.001, date: '2014-10-01'},
     {description: "trader joes", amount: 11, date: '2014-10-02'},
     {description: "albertson", amount: 12, date: '2014-10-03'},
@@ -36,8 +34,18 @@ app.controller('ExpensesViewController', ['$scope', function ($scope) {
     {description: "rite aid", amount: 14, date: '2014-10-05'},
     {description: "pavilion", amount: 15, date: '2014-10-06'}
   ];
+
+  return service;
+});
+
+app.controller('HomeViewController', ['$scope', function ($scope) {
+  $scope.appTitle = "Simple Expenses Tracker";
 }]);
 
-app.controller('ExpenseViewController', ['$scope', '$routeParams', function ($scope, $routeParams) {
-  $scope.someText = "Hello I am Mac Mini. ID = " + $routeParams.id;
+app.controller('ExpensesViewController', ['$scope', 'Expenses', function ($scope, Expenses) {
+  $scope.expenses = Expenses.entries;
+}]);
+
+app.controller('ExpenseViewController', ['$scope', '$routeParams', 'Expenses', function ($scope, $routeParams, Expenses) {
+  $scope.someText = "Hello I am Mac Mini. ID = " + $routeParams.id + " " + Expenses.entries[0].description;
 }]);
